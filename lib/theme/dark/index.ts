b90b8d6b57eb735d4405d23b43ed34508acc1fd9
Andrey Litvinov <drune.email@gmail.com>
2024-07-28 18:01:21 +0300

    ad dark and light theme tokens

---
+++ lib/theme/dark/index.ts
@@ -0,0 +1,16 @@
+import type { Theme } from '../common';
+import { COLOR as color } from './color';
+import { shadow } from './shadow';
+import { zIndex } from '../common/zIndex';
+import { locales } from '../common/locales';
+
+export const DARK_THEME: Theme = {
+  color,
+  shadow,
+  zIndex,
+  name: 'dark',
+  shape: { borderRadiusKind: 'Border radius 4' },
+  currentLocale: 'ru',
+  locales,
+  fontFamily: `'VTB Group UI', sans-serif`,
+};
---
+++ lib/theme/light/index.ts
@@ -0,0 +1,16 @@
+import type { Theme } from '../common';
+import { COLOR as color } from '../light/color';
+import { shadow } from './shadow';
+import { zIndex } from '../common/zIndex';
+import { locales } from '../common/locales';
+
+export const LIGHT_THEME: Theme = {
+  color,
+  shadow,
+  zIndex,
+  name: 'light',
+  shape: { borderRadiusKind: 'Border radius 4' },
+  currentLocale: 'ru',
+  locales,
+  fontFamily: `'VTB Group UI', sans-serif`,
+};
